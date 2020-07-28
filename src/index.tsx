9a86368fdf4d1b15f2bb77e6df15624f6a268215
Allan Carlos <allancarlosdeveloper@gmail.com>
2020-07-28 13:38:53 -0300

    isolate router history

--- src/index.tsx
+++ src/index.tsx
@@ -2,19 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
-import { createBrowserHistory } from "history";
 
-import App from './App';
+import Routes from './routes';
 
 import store from './store/store';
-
-const history = createBrowserHistory();
-
+import history from './services/history';
 
 ReactDOM.render(
   <Provider store={store}>
     <Router history={history}>
-      <App />
+      <Routes />
     </Router>
   </Provider>,
   document.getElementById('root'),
